aeb45a2dcaf11f3c34c059b8ba9d950e69d81b6c
David Yang <david.yang@pdx.edu>
2026-03-31 12:25:47 -0700

    add figure adjustment

--- debug_example_nbe107.py
+++ debug_example_nbe107.py
@@ -5,17 +5,21 @@ from bridge_gym.example_nbe107.rl_env import SingleElement
 from torchrl.envs.utils import check_env_specs
 from torchrl.envs import GymWrapper
 
+
 # %%
 # test gym env
 
 if __name__ == "__main__":
     max_steps, gamma = 50, 1/1.03
-    alpha_vector = np.array([0.14964171, 0.11136174, 0.05003725, 0.03926025])
+    reset_prob = np.array([1.0]+[0.0]*3, dtype=np.float32)
+    alpha_vector = None
+    # reset_prob = None
+    # alpha_vector = np.array([0.14964171, 0.11136174, 0.05003725, 0.03926025])
 
     env = SingleElement(
         max_steps=max_steps, discount=gamma,
         include_step_count=True,
-        reset_prob=None,
+        reset_prob=reset_prob,
         dirichlet_alpha=alpha_vector,
         # render_mode="ansi",
         render_mode="human",
@@ -35,10 +39,8 @@ if __name__ == "__main__":
         # action = 0
 
         # custom action
-        if obs[2] > 0.3 or obs[3] > 0.05:
+        if obs[3] > 0.05:
             action = 3
-        elif obs[2] > 0.1:
-            action = 2
         else:
             action = 0
 
@@ -51,6 +53,34 @@ if __name__ == "__main__":
         action_log.append(action)
         reward_log.append(reward)
         obs = next_obs
+    
+    # reformat the figure using env.fig
+    # change figuze size to 3.25 by 2.5
+    env.fig.set_size_inches(3.25, 2.0)
+
+    # set x label
+
+    # change font size to 9
+    font_size = 8
+    for ax in env.fig.axes:
+        # Update axis labels
+        if ax.xaxis.label:
+            ax.xaxis.label.set_size(font_size)
+        if ax.yaxis.label:
+            ax.yaxis.label.set_size(font_size)
+
+        # Update tick labels
+        ax.tick_params(axis='x', which='major', labelsize=font_size)
+        ax.tick_params(axis='y', which='major', labelsize=font_size)
+
+    # change x label
+    env.fig.axes[0].set_xlabel("Time (years)", fontsize=font_size)
+
+    # clean up the layout so the newly enlarged text doesn't overlap
+    env.fig.tight_layout()
+
+    # env.fig.savefig("../figures/plots/cs_do_nothing.png", dpi=600)
+    # env.fig.savefig("../figures/plots/cs_custom1.png", dpi=600)
 
 # %%
 # test wrapping to torchrl env (must run previous cell first)
